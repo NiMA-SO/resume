a34e4073c07bd0bfc446e0567a2529dee7bf7974
NIMA <nima85sohrabi@gmail.com>
2024-11-04 17:36:22 +0330

    Refactor- sidebar

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import sideBarContext from "./SideBarContext";
 import SideBarMenu from "./SideBarMenu";
 import SideBarLogo from "./SideBarLogo";
