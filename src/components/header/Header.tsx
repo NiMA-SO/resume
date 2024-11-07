5d025f1ff579ff0d27f2279b92b5eb88f265cdd3
NIMA <nima85sohrabi@gmail.com>
2024-11-07 19:51:04 +0330

    fixed h for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   const isMobile = useMediaQuery({ query: "(min-width: 640px)" });
   const isMobileSocial = useMediaQuery({ query: "(min-width: 1000px)" });
   return (
-    <header className="col-span-6 flex justify-between px-2 md:px-8 items-center self-start md:col-span-12 sticky top-0 h-[90px] z-20 bg-[#ffffff] shadow-[0_4px_12px_rgba(0,0,0,0.1)] dark:shadow-[0_4px_12px_rgba(255,255,255,0.05)]">
+    <header className="col-span-6 flex justify-between px-2 md:px-8 items-center self-start md:col-span-12 sticky top-0 h-[10vh] z-20 bg-[#ffffff] shadow-[0_4px_12px_rgba(0,0,0,0.1)] dark:shadow-[0_4px_12px_rgba(255,255,255,0.05)]">
       <HeaderTitle />
       {isMobile && <HeaderSkills />}
       {isMobileSocial && <HeaderSocial />}
